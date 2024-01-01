2e961e97be6925e810cc01cb6049902c8790fa79
kmille <github@androidloves.me>
2024-01-01 17:24:42 +0100

    Squashed commit of the following:
    commit 2cce96c819300620a59689050fa0e981e4754d04
    Author: kmille <github@androidloves.me>
    Date:   Mon Jan 1 17:22:56 2024 +0100
    
        Change UI for transpose functionality
    
    commit 39410cc8953f4244a9bf721e7219660de23af553
    Merge: 6ab9bae c3e3d25
    Author: miguel <miguelmail@gmail.com>
    Date:   Sat Dec 30 10:43:33 2023 +0100
    
        erge remote-tracking branch 'upstream/main'
    
    commit 6ab9baeb0089d7de84fa74351c12cb68d88ce8ea
    Author: Miguel Gonzalez <mgonzalez@uw.co.uk>
    Date:   Wed Dec 13 21:29:10 2023 +0000
    
        Fix selector
    
    commit 4f66432f59a3da5a12a7c710dc10d5d0595c85d9
    Author: Miguel Gonzalez <mgonzalez@uw.co.uk>
    Date:   Wed Dec 13 21:17:42 2023 +0000
    
        More variations
    
    commit eec7755e91717d10f4637d10f26a46266c69a661
    Author: Miguel Gonzalez <mgonzalez@uw.co.uk>
    Date:   Sun Dec 10 18:33:06 2023 +0000
    
        Add flats
    
    commit 1a0b5d1a58b16b912abaae514610a7f07d409b4a
    Author: Miguel Gonzalez <mgonzalez@uw.co.uk>
    Date:   Sun Dec 10 18:14:52 2023 +0000
    
        Simplify
    
    commit 4126baec5cf26561eb872369458701f70790f420
    Author: Miguel Gonzalez <mgonzalez@uw.co.uk>
    Date:   Sun Dec 10 18:13:05 2023 +0000
    
        Better transposition algorithm
    
    commit 8caf736e1ed9451b03a8a394f99caf5bdeca8f3b
    Author: Miguel Gonzalez <mgonzalez@uw.co.uk>
    Date:   Sun Dec 10 17:44:57 2023 +0000
    
        Add transposing and Dockerfile

--- freetar/static/custom.js
+++ freetar/static/custom.js
@@ -5,52 +5,115 @@ function colorize_favs() {
     // make every entry yellow if we faved it before
     favorites = JSON.parse(localStorage.getItem("favorites")) || {};
 
-    $("#results tr").each(function() {
+    $("#results tr").each(function () {
         var tab_url = $(this).find(".song").find("a").attr("href");
         if (favorites[tab_url] != undefined) {
             $(this).find(".favorite").css("color", "#ffae00");
         }
     });
-
 }
 
-$(document).ready(function() {
-/*
-    $(".favorite").click(function() {
-            console.log("fav was clicked");
-            favorites = JSON.parse(localStorage.getItem("favorites")) || {};
-            var row = $(this).closest("tr");
-            var fav = new Map();
+function initialise_transpose() {
+    let transpose_value = 0;
+    //const current = $('#transpose .current')
+    const minus = $('#transpose_down')
+    const plus = $('#transpose_up')
+    //current.text(transpose_value)
+    plus.click(function () {
+        transpose_value = Math.min(11, transpose_value + 1)
+        //current.text(transpose_value)
+        transpose()
+    });
+    minus.click(function () {
+        transpose_value = Math.max(-11, transpose_value - 1)
+        //current.text(transpose_value)
+        transpose()
+    });
 
-            tab_url = row.find(".song").find("a").attr("href");
-            if (favorites[tab_url] != undefined) {
-                delete favorites[tab_url];
-                row.find(".favorite").css("color", "#000000");
-            } else {
+    $('.tab').find('strong').each(function () {
+        const text = $(this).text()
+        $(this).attr('data-original', text)
+    })
 
-                fav["artist_name"] = row.find(".artist").text();
-                fav["tab_url"] = tab_url;
-                fav["song"] = row.find(".song").text();
-                fav["type"] = row.find(".type").text();
-                fav["rating"] = row.find(".rating").text();
-
-                favorites[fav["tab_url"]] = fav;
-               row.find(".favorite").css("color", "#ffae00");
-            }
-
-            localStorage.setItem("favorites", JSON.stringify(favorites));
+    function transpose() {
+        $('.tab').find('strong').each(function () {
+            const text = $(this).attr('data-original')
+            const new_text = transpose_chord(text.trim(), transpose_value)
+            $(this).text(new_text)
         });
-*/
+    }
 
-        colorize_favs();
+    const variations = ['', 'dim', 'm', 'm7', 'maj7', '7', 'sus4', 'sus2', 'sus', 'dim7', 'min7b5', '7sus4', '6', 'm6', '9', 'm9', 'maj9', '11', 'add2', 'add9']
+    const notes = ['C', 'C#', 'D', 'D#', 'E', 'F', 'F#', 'G', 'G#', "A", 'A#', 'B']
+    const notesFlat = ['C', 'Db', 'D', 'Eb', 'E', 'F', 'Gb', 'G', 'Ab', "A", 'Bb', 'B']
 
-        //set dark mode
-        dark_mode = JSON.parse(localStorage.getItem("dark_mode")) || false;
-        if (dark_mode) {
-            document.documentElement.setAttribute('data-bs-theme','dark')
+    const chords = notes.map(note => variations.map(variation => note + variation))
+    const chordsFlat = notesFlat.map(note => variations.map(variation => note + variation))
+        
+
+    function transpose_chord(chord, transpose_value) {
+        if (chord.indexOf('/') !== -1) {
+            const transposed = chord.split('/').map(cho => transpose_chord(cho, transpose_value))
+            return transposed.join('/')
         }
-});
+        let chordGroup = chords;
+        let chord_index = chords.findIndex(chordGroup => chordGroup.includes(chord))
+        if (chord_index === -1) {
+            chordGroup = chordsFlat;
+            chord_index = chordsFlat.findIndex(chordGroup => chordGroup.includes(chord))
+            if (chord_index === -1) {
+                return chord
+            }
+        }
+        let new_index = (chord_index + transpose_value) % 12
+        if (new_index < 0) {
+            new_index += 12
+        }
 
+        return chordGroup[new_index][chordGroup[chord_index].indexOf(chord)]
+    }
+}
+
+
+$(document).ready(function () {
+    /*
+        $(".favorite").click(function() {
+                console.log("fav was clicked");
+                favorites = JSON.parse(localStorage.getItem("favorites")) || {};
+                var row = $(this).closest("tr");
+                var fav = new Map();
+    
+                tab_url = row.find(".song").find("a").attr("href");
+                if (favorites[tab_url] != undefined) {
+                    delete favorites[tab_url];
+                    row.find(".favorite").css("color", "#000000");
+                } else {
+    
+                    fav["artist_name"] = row.find(".artist").text();
+                    fav["tab_url"] = tab_url;
+                    fav["song"] = row.find(".song").text();
+                    fav["type"] = row.find(".type").text();
+                    fav["rating"] = row.find(".rating").text();
+    
+                    favorites[fav["tab_url"]] = fav;
+                   row.find(".favorite").css("color", "#ffae00");
+                }
+    
+                localStorage.setItem("favorites", JSON.stringify(favorites));
+            });
+    */
+
+    colorize_favs();
+
+    //set dark mode
+    dark_mode = JSON.parse(localStorage.getItem("dark_mode")) || false;
+    if (dark_mode) {
+        document.documentElement.setAttribute('data-bs-theme', 'dark')
+    }
+
+    initialise_transpose();
+
+});
 
 function pageScroll() {
     console.log(scroll_timeout);
@@ -60,14 +123,14 @@ function pageScroll() {
     }
 }
 
-$('#checkbox_autoscroll').click(function(){
-if($(this).is(':checked')){
-    do_scroll = true;
-    pageScroll();
-} else {
-    scroll_timeout = 500;
-    do_scroll = false;
-}
+$('#checkbox_autoscroll').click(function () {
+    if ($(this).is(':checked')) {
+        do_scroll = true;
+        pageScroll();
+    } else {
+        scroll_timeout = 500;
+        do_scroll = false;
+    }
 });
 
 $('#checkbox_view_chords').click(function(){
@@ -80,11 +143,11 @@ $('#checkbox_view_chords').click(function(){
 
 $('#dark_mode').click(function(){
     if (document.documentElement.getAttribute('data-bs-theme') == 'dark') {
-        document.documentElement.setAttribute('data-bs-theme','light');
+        document.documentElement.setAttribute('data-bs-theme', 'light');
         localStorage.setItem("dark_mode", false);
     }
     else {
-        document.documentElement.setAttribute('data-bs-theme','dark');
+        document.documentElement.setAttribute('data-bs-theme', 'dark');
         localStorage.setItem("dark_mode", true);
     }
 });
