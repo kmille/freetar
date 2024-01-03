d583510e928aad72bc778920c40645e006c381b3
Yo'av Moshe <github@yoavmoshe.com>
2024-01-03 23:00:16 +0100

    use favorites as homepage

--- freetar/backend.py
+++ freetar/backend.py
@@ -8,7 +8,10 @@ app = Flask(__name__)
 
 @app.route("/")
 def index():
-    return render_template("index.html")
+    return render_template("index.html",
+                           title="Freetar",
+                           favs=True)
+
 
 
 @app.route("/search")
