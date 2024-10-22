f0ab6a38e4b1141b30c0843b338d4973b0e2cdb0
kmille <github@androidloves.me>
2024-10-22 22:27:33 +0200

    Enable Tor support
    When env FREETAR_ENABLE_TOR=1 is set, use socks5://localhost:9050 proxy
    when sending requests to UG. Workaround, as UG started to block ips (403
    cloudflare).

--- README.md
+++ README.md
@@ -2,6 +2,8 @@
 
 This is like [Invidious](https://invidious.io/) but only for [Ultimate Guitar](https://www.ultimate-guitar.com/).  
 
+**UPDATE 22.10.2023:** As ultimate-guitar.com started to block (some? my? server?) ip addresses it's now possible to send requests to UG over Tor (socks5 proxy listening on `localhost:9050`). This feature can be enabled when environment variable `FREETAR_ENABLE_TOR=1` is set. Supported since Freetar version 0.10.0.
+
 ## Instances
 - https://freetar.de
 - https://freetar.habedieeh.re
--- freetar/ug.py
+++ freetar/ug.py
@@ -3,12 +3,19 @@ from bs4 import BeautifulSoup
 from urllib.parse import quote, urlparse
 import json
 import re
+import os
 
 from dataclasses import dataclass, field
 from .utils import FreetarError
 
 
 USER_AGENT = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/125.0.0.0 Safari/537.3"
+
+if os.environ.get("FREETAR_ENABLE_TOR", "") != "":
+    print("Enabling tor for requests to ultimate guitar")
+    PROXIES = {'https': 'socks5://127.0.0.1:9050'}
+else:
+    PROXIES = None
 
 
 @dataclass
@@ -102,7 +109,8 @@ class SongDetail():
 def ug_search(value: str):
     try:
         resp = requests.get(f"https://www.ultimate-guitar.com/search.php?search_type=title&value={quote(value)}",
-                            headers={'User-Agent': USER_AGENT})
+                            headers={'User-Agent': USER_AGENT},
+                            proxies=PROXIES)
         resp.raise_for_status()
         bs = BeautifulSoup(resp.text, 'html.parser')
         # data can be None
@@ -182,7 +190,8 @@ def get_chords(s: SongDetail) -> SongDetail:
 def ug_tab(url_path: str):
     try:
         resp = requests.get("https://tabs.ultimate-guitar.com/tab/" + url_path,
-                            headers={'User-Agent': USER_AGENT})
+                            headers={'User-Agent': USER_AGENT},
+                            proxies=PROXIES)
         resp.raise_for_status()
         bs = BeautifulSoup(resp.text, 'html.parser')
         data = bs.find("div", {"class": "js-store"})
